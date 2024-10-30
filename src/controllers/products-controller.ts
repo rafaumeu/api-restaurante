e9362b90722fcf8425e8106275e36d6131008f95
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-30 07:47:10 -0300

    validação de dados para cadastrar um produto.

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,11 +1,30 @@
-import { AppError } from '@/utils/AppError';
 import { NextFunction, Request, Response } from 'express';
+import {z} from 'zod';
+
 
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       
       return response.json({ message: 'ok' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0)
+      })
+      const { name, price } = bodySchema.parse(request.body);
+      return response.status(201).json(
+        {
+          name,
+          price
+        }
+      );
     } catch (error) {
       next(error);
     }
