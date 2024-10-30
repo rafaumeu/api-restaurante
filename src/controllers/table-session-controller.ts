6d0e664e32e984d122a558b37578261d6e9881c3
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-30 09:24:47 -0300

    criação de sessões de tabela

--- src/controllers/table-session-controller.ts
+++ src/controllers/table-session-controller.ts
@@ -1,10 +1,19 @@
+import { knex } from '@/database/knex'
 import { Request, Response, NextFunction } from 'express'
-import knex from 'knex'
+
+import {z} from 'zod'
 class TableSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
-     
-      return response.json({message: 'ok'})
+      const bodySchema = z.object({
+        table_id: z.number()
+      })
+
+      const {table_id} = bodySchema.parse(request.body)
+      
+
+      await knex<TableSessionsRepository>('table_sessions').insert({table_id, opened_at: knex.fn.now()})
+      return response.status(201).json()
     } catch (error) {
       next(error)
     }
