6a942bae2bc224af15036ceca2c6e6de620a37b2
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-30 08:09:14 -0300

    listando produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -6,8 +6,13 @@ import {z} from 'zod';
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
+      const {name} = request.query;
+      const products = await knex<ProductRepository>('products')
+      .select()
+      .whereLike('name', `%${name ?? ""}%`)
+      .orderBy('name');
       
-      return response.json({ message: 'ok' });
+      return response.json(products);
     } catch (error) {
       next(error);
     }
