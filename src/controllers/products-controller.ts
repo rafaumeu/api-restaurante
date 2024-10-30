15df2984a7fdcafd326914e10cd4c6e3eaef989f
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-30 08:20:12 -0300

    atualizando produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -31,6 +31,28 @@ class ProductsController {
       next(error);
     }
   }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+      .string()
+      .transform((value) => Number(value))
+      .refine((value) => !isNaN(value), { message: 'id must be a number' })
+      .parse(request.params.id);
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0)
+      })
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      await knex<ProductRepository>('products').update({ name, price, updated_at: knex.fn.now() }).where('id', id);
+      return response.status(204).json();
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { ProductsController }
