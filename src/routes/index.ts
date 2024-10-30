80450b647acbe8ca0fd39e251adffe2b23ba20aa
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-30 10:50:27 -0300

    criação das validações do controller criação  da rota de pedidos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { productsRoutes } from "./product-routes";
 import { tablesRoutes } from "./tables-routes";
 import { tablesSessionsRoutes } from "./tables-sessions-routes";
+import { ordersRoutes } from "./order-routes";
 
 const routes = Router();
 
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
 routes.use("/tables-sessions", tablesSessionsRoutes);
+routes.use("/orders", ordersRoutes);
 
 export { routes };
